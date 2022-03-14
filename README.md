47e9582311895f5cbe5d2470d2cd741b499f2f14
thsmale <60244532+thsmale@users.noreply.github.com>
2022-03-13 22:03:06 -0700

    Upload files from . directory

---
+++ README.md
@@ -0,0 +1,3 @@
+# website-distill
+Starter code for a Distill based website
+Additional resources: https://rstudio4edu.github.io/rstudio4edu-book/make-distill.html
---
+++ render_site.R
@@ -0,0 +1,3 @@
+rmarkdown::render("_posts/2022-02-01-find-the-giraffe/find-the-giraffe.Rmd")
+rmarkdown::render("_posts/2022-02-01-how-to-be-an-influencer/how-to-be-an-influencer.Rmd")
+rmarkdown::render_site()
